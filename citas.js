df69e63bfa8b0a81d20745558ee1cadccaa68f2f
cavaldezp <cavaldezp@gmail.com>
2021-04-24 22:55:29 -0500

    version2

--- citas.js
+++ citas.js
@@ -14,23 +14,25 @@ while (prompt(console.log("¿Tratamiento en Curso? Ingrese S mayuscula para cont
     if(NC<=3){
         console.log(`El costo de la cita es S/. 200.00`);
         TOT=TOT+200;
-        console.log(`El costo total de tratamientos es ${TOT}`);
+        console.log(`El costo total de tratamientos a la fecha es S/. ${TOT}`);
     }else if((NC>3)&&(NC<6)){
         console.log(`El costo de la cita es S/. 150.00`);
         TOT=TOT+150;
-        console.log(`El costo total de tratamientos es ${TOT}`);
+        console.log(`El costo total de tratamientos a la fecha es S/. ${TOT}`);
     }else if((NC>=6)&&(NC<=8)){
         console.log(`El costo de la cita es S/. 100.00`);
         TOT=TOT+100;
-        console.log(`El costo total de tratamientos es ${TOT}`);
+        console.log(`El costo total de tratamientos a la fecha es S/. ${TOT}`);
     }else{
         console.log(`El costo de la cita es S/. 50.00`);
         TOT=TOT+50;
-        console.log(`El costo total de tratamientos es ${TOT}`);
+        console.log(`El costo total de tratamientos a la fecha es S/. ${TOT}`);
     }
 
 
-
 }
+console.log(`Fin del tratamiento`);
+console.log(`El número total de citas es ${NC}`);
+console.log(`El costo total final del tratamientos es S/. ${TOT}`);
 
         
